462d26c85398f3e4bd366915f7c4856aa01ff153
Kamil Kisielewicz <kamiljkisielewicz@gmail.com>
2023-03-16 22:30:59 -0700

    update npm pkg version

--- ml-feedback-widget/src/index.js
+++ ml-feedback-widget/src/index.js
@@ -1,89 +1,85 @@
 import React, { useState } from 'react';
-import { ThumbsDown, ThumbsUp } from 'react-feather';
+import { CheckCircle, ThumbsDown, ThumbsUp } from 'react-feather';
 
 import axios from 'axios';
 
 import './index.css';
 import TextareaAutosize from 'react-textarea-autosize';
 
-function MLFeedbackWidget( { widgetDescriptionText, postSubmissionText, url }) {
+function MLFeedbackWidget( { widgetDescriptionText, postSubmissionText, placeholderText, feedback_endpoint_url }) {
     const [feedback, setFeedback] = useState('');
     const [isFeedbackSent, setIsFeedbackSent] = useState(false);
     const [hasBeenLiked, setHasBeenLiked] = useState(false);
     const [isLiked, setIsLiked] = useState(false);
+
+    const fill_dislike = (hasBeenLiked && !isLiked) ? '#f44336' : 'none';
+    const fill_like = (hasBeenLiked && isLiked) ? '#4CAF50' : 'none';
 
     const handleFeedbackChange = (event) => {
         setFeedback(event.target.value);
     }
 
     const sendFeedback = async () => {
-        if (url){
-            const config = {
-                headers: {
-                  'Content-Type': 'application/json'
-                }
-              }
-            const data = {
-                isLiked: isLiked,
-                feedback: feedback,
+        const config = {
+            headers: {
+              'Content-Type': 'application/json'
             }
-            axios.post(url, data, config)
-                .then((response) => {
-                    console.log(response);
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
+          }
+        const data = {
+            isLiked: isLiked,
+            feedback: feedback,
         }
+        axios.post(feedback_endpoint_url, data, config)
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
 
     // TODO: maybe add logic to handle double clicking
     const like = () => {
         setHasBeenLiked(true);
         setIsLiked(true);
-        sendFeedback();
     }
 
     // TODO: maybe add logic to handle double clicking
     const dislike = () => {
         setHasBeenLiked(true);
         setIsLiked(false);
-        sendFeedback();
     }
 
     const handleFeedbackSubmission = (event) => {
         event.preventDefault();
+        sendFeedback();
         setHasBeenLiked(false);
         setIsFeedbackSent(true);
-        sendFeedback();
     }
-
-    const fill_dislike = (hasBeenLiked && !isLiked) ? '#f44336' : 'none';
-    const fill_like = (hasBeenLiked && isLiked) ? '#4CAF50' : 'none';
-
 
     return (
         <div>
-            <div class="container">
+            <div class="ml-widget-container">
                 {isFeedbackSent ?
-                    <div>
-                        <p class="post-submission-text">{postSubmissionText}</p>
+                    <div class="ml-widget-post-submission">
+                        <CheckCircle color={'green'} size={18} />
+                        <p class="ml-widget-post-submission-text">{postSubmissionText}</p>
                     </div>
                     :
                     <div>
-                        <p class="widget-description-text">{widgetDescriptionText}</p>
-                        <div class="buttons">
-                            <button class="like-button" onClick={like}>
-                                <ThumbsUp fill={fill_like} />
+                        <p class="ml-widget-description-text">{widgetDescriptionText}</p>
+                        <div class="ml-widget-buttons">
+                            <button class="ml-widget-like-button" onClick={like}>
+                                <ThumbsUp fill={fill_like} size={18}/>
                             </button>
-                            <button class="dislike-button" onClick={dislike}>
-                                <ThumbsDown fill={fill_dislike} />
+                            <button class="ml-widget-dislike-button" onClick={dislike}>
+                                <ThumbsDown fill={fill_dislike} size={18}/>
                             </button>
                         </div>
                         {hasBeenLiked && 
-                            <form class="feedback-form" onSubmit={handleFeedbackSubmission}>
-                                <TextareaAutosize class="text-box" placeholder="Please provide additional feedback" value={feedback} onChange={handleFeedbackChange} />
-                                <input type="submit" class="submit-button" value="Submit" />
+                            <form class="ml-widget-feedback-form" onSubmit={handleFeedbackSubmission}>
+                                <TextareaAutosize class="ml-widget-text-box" placeholder={placeholderText} value={feedback} onChange={handleFeedbackChange} />
+                                <input type="submit" class="ml-widget-submit-button" value="Submit" />
                             </form>
                         }
                     </div>
@@ -97,5 +93,6 @@ export default MLFeedbackWidget;
 
 MLFeedbackWidget.defaultProps = {
     widgetDescriptionText: 'Are these results relevant?',
-    postSubmissionText: 'Thank you for your feedback!'
+    postSubmissionText: 'Thanks for the feedback!',
+    placeholderText: 'Your feedback (optional)'
 }
